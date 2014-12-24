915877e3438c688a82110dd4bf51f9c6cbce0f18
ahmad saufwan sazali <asfwan@gmail.com>
2014-12-24 22:51:29 +0800

    added the rest of files

--- app/src/main/java/com/fyp/fatin/iqra_app/IqraActivity_Main.java
+++ app/src/main/java/com/fyp/fatin/iqra_app/IqraActivity_Main.java
@@ -2,6 +2,7 @@ package com.fyp.fatin.iqra_app;
 
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
@@ -16,45 +17,85 @@ public class IqraActivity_Main extends IqraActivity_Core {
         super.onCreate(savedInstanceState);
 
         // setup the cards
-        setupCards();
+        View dorobaCard = getDorobaCard();
+        View zoharoCard = getZoharoCard();
+
+        mLayout.addView(dorobaCard);
+        mLayout.addView(zoharoCard);
 
     }
 
-    // the function definition for setupCards()
-    private void setupCards() {
+    private View getDorobaCard() {
 
-
-        LinearLayout imageLayout = getImageFromResource(R.drawable.doroba,
-                ImageComponentsManager.convertToOnTouchListener(onDorobaTouchListener));
-
-        mLayout.addView(imageLayout);
-
-    }
-
-
-
-    OnComponentTouchListener onDorobaTouchListener = new OnComponentTouchListener() {
-        @Override
-        public void onTouch(ImageView v, int componentCode) {
-            switch (componentCode) {
-                case 1:
-                    showImageDialog(R.drawable.ba);
-                    playSound(R.raw.baa);
-                    v.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.doroba_ba_red));
-                    break;
-                case 2:
-                    showImageDialog(R.drawable.ro);
-                    playSound(R.raw.raa);
-                    v.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.doroba_ro_red));
-                    break;
-                case 3:
-                    showImageDialog(R.drawable.dho);
-                    playSound(R.raw.doo);
-                    v.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.doroba_do_red));
-                    break;
+        OnComponentTouchListener onDorobaComponentTouchListener = new OnComponentTouchListener() {
+            @Override
+            public void onTouch(ImageView v, int componentCode) {
+                switch (componentCode) {
+                    case 1:
+                        showImageDialog(R.drawable.ba);
+                        playSound(R.raw.baa);
+                        v.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.doroba_ba_red));
+                        break;
+                    case 2:
+                        showImageDialog(R.drawable.ro);
+                        playSound(R.raw.raa);
+                        v.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.doroba_ro_red));
+                        break;
+                    case 3:
+                        showImageDialog(R.drawable.dho);
+                        playSound(R.raw.doo);
+                        v.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.doroba_do_red));
+                        break;
+                }
             }
-        }
-    };
+        };
+
+        View.OnTouchListener onDorobaTouchListener =
+                ImageComponentsManager.convertToOnTouchListener(onDorobaComponentTouchListener);
+
+        LinearLayout doroba = getImageFromResource(R.drawable.doroba, onDorobaTouchListener);
+
+        return doroba;
+
+    }
+
+    private View getZoharoCard() {
+
+        OnComponentTouchListener onZoharoComponentTouchListener = new OnComponentTouchListener() {
+            @Override
+            public void onTouch(ImageView v, int componentCode) {
+                switch (componentCode) {
+                    case 1:
+                        showImageDialog(R.drawable.ro);
+                        playSound(R.raw.raa);
+                        v.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.dzaharo_ro_red));
+                        break;
+                    case 2:
+                        showImageDialog(R.drawable.ha);
+                        playSound(R.raw.haa);
+                        v.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.dzaharo_ha_red));
+                        break;
+                    case 3:
+                        showImageDialog(R.drawable.dzo);
+                        playSound(R.raw.dzo);
+                        v.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.dzaharo_dzo_red));
+                        break;
+                }
+            }
+        };
+
+        View.OnTouchListener onZoharoTouchListener =
+                ImageComponentsManager.convertToOnTouchListener(onZoharoComponentTouchListener);
+
+        LinearLayout zoharo = getImageFromResource(R.drawable.dzaharo, onZoharoTouchListener);
+
+        return zoharo;
+
+    }
+
+
+
+
 }
 
 
